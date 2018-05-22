b94c9a680540b12a57e8c37d299f5df720a605ea
Georg Steyn <georgsteyn@gmail.com>
2018-05-22 13:56:24 +0200

    Added auto updater and installer with uninstaller

--- frontend/src/app/shared/constants/constants.ts
+++ frontend/src/app/shared/constants/constants.ts
@@ -1,8 +1,8 @@
 export const wsURL = window.location.origin.includes('https') ? 'wss://' + window.location.host + '/ws' : 'ws://127.0.0.1:9000/ws';
 
-export const version = '0.1.1'
+export const version = '0.2.0'
 
-export const releaseDate = '2018-05-18'
+export const releaseDate = '2018-05-22'
 
 export const URL = 'http://127.0.0.1:9000/';
 
