617fac365c8db0746c9ed096758051974760eb6a
Margrit Drescher <margrit2103@gmail.com>
2020-03-09 12:58:27 +0200

    [Test Runner] simplify process
    1. parameter LogName in conf file, allowing trace of runner.py execution
    2. Replace lackey pattern recognition with  pyautogui .LocateOnScreen.  Use grayscale - 30% speed improvement.  Add loop to reduce confidence level (without grayscale) if not found on first attempt
    3. Attempt to improve error message handling

--- apps/test_runner/main.py
+++ apps/test_runner/main.py
@@ -19,8 +19,11 @@ else:
     APP = {
         'status': 'Starting',
         'save_folder': os.path.normpath(os.getenv("PROGRAMDATA") + '/TingusData' + '/save_files/'),
-        'settings_file': os.path.normpath('C:/ProgramData/TingusData/save_files/settings.json')
+        'settings_file': os.path.normpath('C:/ProgramData/TingusData/save_files/settings.json'),
+        
     }
+    
+    
 
     parser = argparse.ArgumentParser(description='Tingus Start Options.')
     parser.add_argument('run_test', help='Name of the Test Case or Test Suite to run', type=str)
@@ -31,6 +34,8 @@ else:
     parser.add_argument('--save', help='Save directory path of Tingus data', type=str)
     parser.add_argument('--log_name', help='Define the name of the log', type=str)
 
+
+    
     logging.info('Reading command line input...')
     if not len(sys.argv) > 1:
         parser.print_help()
@@ -38,18 +43,17 @@ else:
 
     args = parser.parse_args()
     APP['run_test'] = args.run_test
-    APP['test_type'] = args.test_type
+    APP['test_type'] = args.test_type         
     if args.settings:
         APP['settings_file'] = args.settings
     if args.save:
         APP['save_folder'] = args.save
-    if args.log_name:
-        APP['log_name'] = args.log_name
+
     if args.test_type not in ['test', 'suite']:
         logging.error('Test type should be testcase or testsuite you entered: {}'.format(args.test_type))
         parser.exit()
 
-    APP['settings'] = json.load(open(APP['settings_file']))
+    APP['settings'] = json.load(open(APP['settings_file']))    
 
     logging.info('Running test engine...')
     runner.Runner(APP)
--- apps/test_runner/runner.py
+++ apps/test_runner/runner.py
@@ -7,42 +7,41 @@ import errno
 import subprocess
 
 import pyautogui
-from lackey import click as _click
-from lackey import doubleClick as _doubleClick
-from lackey import rightClick as _rightClick
-from lackey import wait as _wait
-from lackey import waitVanish
-from lackey import Pattern
-import psycopg2
+#from lackey import click as _click
+#from lackey import doubleClick as _doubleClick
+#from lackey import rightClick as _rightClick
+#from lackey import wait as _wait
+#from lackey import waitVanish
+#from lackey import Pattern
+##import psycopg2
 
 
 
 
 
 
-logging.basicConfig(format='%(levelname)s: %(asctime)s %(message)s', level=logging.INFO)
 
 
 
-class MySemaphore:
-    def __init__(self):   
-        self.process_id = 112    
-        self.pg_conn_str = """dbname={dbname} user={user} password={password} host={host} port={port}
-             """.format(dbname='medwell_sa', user='postgres', password="masterkey", host="localhost", port="5412")
-        conn = psycopg2.connect( self.pg_conn_str )
-        cur = conn.cursor()
-        cur.execute( 'create table if not exists tingus_semaphore( process_id integer primary key , busy integer, comment jsonb ) ' )
-        conn.commit()
-        conn.close()
+# class MySemaphore:
+    # def __init__(self):   
+        # self.process_id = 112    
+        # self.pg_conn_str = """dbname={dbname} user={user} password={password} host={host} port={port}
+             # """.format(dbname='medwell_sa', user='postgres', password="masterkey", host="localhost", port="5412")
+        # conn = psycopg2.connect( self.pg_conn_str )
+        # cur = conn.cursor()
+        # cur.execute( 'create table if not exists tingus_semaphore( process_id integer primary key , busy integer, comment jsonb ) ' )
+        # conn.commit()
+        # conn.close()
 
-    def update_status ( self, mystatus, comment ):
-        conn = psycopg2.connect( self.pg_conn_str )
-        cur = conn.cursor()
-        cur.execute( """insert into tingus_semaphore( process_id, busy,comment ) values( 11, {x_status}, '{x_comment}' ) 
-                             on conflict (process_id) do update set busy  = {x_status}, comment = tingus_semaphore.comment||'{x_comment}'
-                                   """.format(x_status=mystatus, x_comment = comment) )
-        conn.commit()
-        conn.close()
+    # def update_status ( self, mystatus, comment ):
+        # conn = psycopg2.connect( self.pg_conn_str )
+        # cur = conn.cursor()
+        # cur.execute( """insert into tingus_semaphore( process_id, busy,comment ) values( 11, {x_status}, '{x_comment}' ) 
+                             # on conflict (process_id) do update set busy  = {x_status}, comment = tingus_semaphore.comment||'{x_comment}'
+                                   # """.format(x_status=mystatus, x_comment = comment) )
+        # conn.commit()
+        # conn.close()
     
 
 
@@ -51,9 +50,22 @@ class Runner:
     """
     def __init__(self, app_data):
         self.data = app_data
+        _logfilename = self.data['settings'].get("testSettings", {}).get("logName", "")
+        #print( 'logfilename %s '% (_logfilename ) )
+        if _logfilename != "":
+            logging.info('Progress will be logged in file %s'%(_logfilename )  )       
+            logid = logging.getLogger()
+            for hndl in logid.handlers[:]:
+                logid.removeHandler( hndl )
+            logging.basicConfig(filename=_logfilename,format='%(levelname)s: %(asctime)s %(message)s', level=logging.INFO)           
+        else:    
+            logging.basicConfig(format='%(levelname)s: %(asctime)s %(message)s', level=logging.INFO)           
+                
+        logging.info( '------------------')
+        
         self.__data_init__()
         self.FileHandling = FileHandling()
-        self.sema = MySemaphore()
+        #self.sema = MySemaphore()
         self.run_test(self.data['run_test'], self.data['test_type'])
 
     def __data_init__(self):
@@ -66,12 +78,33 @@ class Runner:
         result = self._run_test(test_name, test_type)
         with self.FileHandling.safe_open_w(os.path.normpath(self.data['save_folder'] + '/logs/' + self.log_name + '.json')) as fp:
             json.dump(result, fp, indent=4)
-
+    
+    
+    def _test_name( self,  default_folder , sub_directory, test_name ):
+        _testname = os.path.normpath(test_name)
+        #print ( format( '1. %s  2. %s '%(test_name, _testname )) )
+        _name, _ext  = os.path.splitext( _testname )
+        #print (  '_name. %s  _ext. %s '%(_name, _ext ) )
+        if _ext != '.json':
+            _testname = '%s%s'%(_name,'.json' )
+            #print( ' ext test %s '%(_testname))
+        if os.path.isfile( _testname ):
+            return _testname
+        else:    
+            _testname= os.path.normpath( default_folder + sub_directory + _testname  )       
+            #print( ' test 2  %s '%(_testname))
+            if os.path.isfile( _testname ):
+                return _testname
+            else:
+                logging.error(  'Test file not found %s ',_testname  )
+                raise FileNotFoundError
+          
+       
 
     def _run_test(self, test_name, test_type):
-        self.sema.update_status( 1, '{"started":"now"}' )
+        #self.sema.update_status( 1, '{"started":"now"}' )
         results = []
-        print( 'test_type %s '%(test_type) )
+        #print( 'test_type %s '%(test_type) )
         if test_type == 'suite':
             try:
                 json_data = json.load(open(os.path.normpath(self.data['save_folder'] + '/suites/' + test_name + '.json')))
@@ -91,7 +124,9 @@ class Runner:
 
         elif test_type == 'test':
             try:
-                json_data = json.load(open(os.path.normpath(self.data['save_folder'] + '/tests/' + test_name + '.json')))
+                #json_data = json.load(open(os.path.normpath(self.data['save_folder'] + '/tests/' + test_name + '.json')))
+                #print(  self._test_name(self.data['save_folder'], '/tests/',test_name) )  
+                json_data = json.load(open( self._test_name(self.data['save_folder'], '/tests/',test_name) ))
             except FileNotFoundError:
                 logging.error('The test does not exist or the defined test type is incorrect or the save_folder path is not correct.')
                 exit()
@@ -111,41 +146,78 @@ class Runner:
             'failed_actions': [],
             'success_actions': []
         }
-
+        _found = 0    
+        _confidence = 0.9
         for index, action in enumerate(model['actions']):
-            print ( ' ACTION index %s ___action %s '%(index, action ) )
+            _found = 1
+            #print ( ' ACTION index %s ___action %s '%(index, action ) )
             if action['action'] in ['click', 'rclick', 'doubleclick', 'wait', 'clickwait', 'waitvanish']:
                 image_meta = ImageJson(action['data'], self.data['save_folder'])
-                print( '    1. MARGRIT index %s ___action %s '%(index, action ) ) 
                 action_delay = int(action['delay']) + self.data['settings'].get("testSettings", {}).get('actionDelayOffset', 0)
-                print( '    2. MARGRIT index %s ___action %s '%(index, action ) ) 
             try:
-                print( '    3. MARGRIT index %s ___action %s '%(index, action ) ) 
                 if action['action'] == 'click':
+                    _image_path =  os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png' ) 
+                    logging.info( 'Looking for pattern %s delay %s' % ( _image_path, action_delay ))
                     for _ in range(int(action.get('repeat', '1'))):
-                        _wait(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png'), action_delay)
-                        _click(Pattern(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png')).targetOffset(image_meta.get_click_offset()[0], image_meta.get_click_offset()[1]))
+                        
+                        #logging.info( 'Looking for pattern %s ' % ( os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png' ) ))
+                        #_wait(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png'), action_delay)
+                        #_click(Pattern(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png')).targetOffset(image_meta.get_click_offset()[0], image_meta.get_click_offset()[1]))
+                        #pyautogui.click( pyautogui.locateCenterOnScreen( 'C:/ProgramData/TingusData/save_files/images/meddeb_inv_btn.png', confidence = 0.5 ) )
+                        #logging.info( 'Looking for pattern %s delay %s' % ( _image_path, action_delay ))
+                        #_wait(_image_path, action_delay)
+                        time.sleep( action_delay)
+                        #logging.info( 'after delay' )
+                        place = pyautogui.locateOnScreen( _image_path, confidence = _confidence,grayscale=True )  # grayscale a bit faster.
+                        _my_confidence = _confidence
+                        while ( place == None ) and ( _my_confidence > 0.5 ):                            
+                            logging.info( ' !!!!!! Reducing confidence level for %s .  Confidence level %s '%(_image_path,_my_confidence) )
+                            place = pyautogui.locateOnScreen( _image_path, confidence = _my_confidence, grayscale = False )
+                            _my_confidence = _my_confidence- 0.1
+                        
+                        if place == None:
+                            #pyautogui.alert( text = _errmess, title = 'Critical error', button = 'OK' )
+                            #logging.info( _errmess )
+                            _errmess = "Cannot locate image %s. Please check that your application is located on the primary screen.\nYour image file might also be out of date." % (_image_path)                                
+                            raise NameError( _errmess )
+                        else:    
+                            logging.info( 'clicking image %s.  Confidence level %s. '%(_image_path,_my_confidence) )
+                        pyautogui.click( pyautogui.center( place ) )
+                        
                 elif action['action'] == 'rclick':
                     for _ in range(int(action.get('repeat', '1'))):
-                        _wait(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png'), action_delay)
-                        _rightClick(Pattern(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png')).targetOffset(image_meta.get_click_offset()[0], image_meta.get_click_offset()[1]))
+                        _image_path =  os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png' ) 
+                        _wait(_image_path, action_delay)
+                        place = pyautogui.locateCenterOnScreen( _image_path, confidence = _confidence )
+                        pyautogui.rightClick( place )
+                        #_wait(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png'), action_delay)
+                        #_rightClick(Pattern(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png')).targetOffset(image_meta.get_click_offset()[0], image_meta.get_click_offset()[1]))
                 elif action['action'] == 'doubleclick':
                     for _ in range(int(action.get('repeat', '1'))):
-                        _wait(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png'), action_delay)
-                        _doubleClick(Pattern(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png')).targetOffset(image_meta.get_click_offset()[0], image_meta.get_click_offset()[1]))
+                        _image_path =  os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png' ) 
+                        _wait(_image_path, action_delay)
+                        place = pyautogui.locateCenterOnScreen( _image_path, confidence = _confidence )
+                        pyautogui.doubleClick( place )                    
+                        #_wait(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png'), action_delay)
+                        #_doubleClick(Pattern(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png')).targetOffset(image_meta.get_click_offset()[0], image_meta.get_click_offset()[1]))
                 elif action['action'] == 'wait':
                     for _ in range(int(action.get('repeat', '1'))):
                         _wait(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png'), action_delay)
                 elif action['action'] == 'clickwait':
                     for _ in range(int(action.get('repeat', '1'))):
-                        _wait(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png'), action_delay)
-                        _click(Pattern(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png')).targetOffset(image_meta.get_click_offset()[0], image_meta.get_click_offset()[1]))
+                        #_wait(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png'), action_delay)
+                        #_click(Pattern(os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png')).targetOffset(image_meta.get_click_offset()[0], image_meta.get_click_offset()[1]))
+                        _image_path =  os.path.normpath(self.data['save_folder'] + '/images/' + action['data'] + '.png' ) 
+                        _wait(_image_path, action_delay)                        
+                        place = pyautogui.locateCenterOnScreen( _image_path, confidence = _confidence )
+                        pyautogui.click( place )                              
                 elif action['action'] == 'type':
                     for _ in range(int(action.get('repeat', '1'))):
                         pyautogui.typewrite(action['data'])
                 elif action['action'] == 'keycombo':
                     keys = action['data'].split('+')
                     for _ in range(int(action.get('repeat', '1'))):
+                        #print ( *keys )
                         pyautogui.hotkey(*keys)
                 elif action['action'] == 'keypress':
                     for _ in range(int(action.get('repeat', '1'))):
@@ -174,9 +246,8 @@ class Runner:
                 })
             except Exception as ex:
                 
-                print( '4.MARGRIT  ') 
                 logging.error(ex)
-                self.sema.update_status( 0, '{"error": "%s", "exit_code": "-1"}'%(str(ex.__doc__)) )
+                #self.sema.update_status( 0, '{"error": "%s", "exit_code": "-1"}'%(str(ex.__doc__)) )
                 test_result['failed_actions'].append({
                     'index': index,
                     'action': action['action'],
@@ -185,7 +256,10 @@ class Runner:
                 })
                 sys.exit( -1 )
                 return 
-        self.sema.update_status( 0, '{ "exit_code": "0"}' ) 
+        #self.sema.update_status( 0, '{ "exit_code": "0"}' ) 
+        
+        if _found == 0:
+            logging.error( 'Unknown action % '%(model['actions']))
         sys.exit( 0 )
         return test_result
 
