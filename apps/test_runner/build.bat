65dbb96793709fbddb0a894b622250e394d65bd4
Margrit Drescher <margrit2103@gmail.com>
2020-04-17 16:38:22 +0200

    Build.bat: Not longer import lacky, not used anymore
    Signed-off-by: Margrit Drescher <margrit2103@gmail.com>

--- apps/test_runner/build.bat
+++ apps/test_runner/build.bat
@@ -1,1 +1,2 @@
-"F:\Program Files\Python37\Scripts\pyinstaller.exe" -y --clean --workpath .\build_temp --name test_runner --onedir --uac-uiaccess --hidden-import lackey --hidden-import pyautogui main.py
+rem "F:\Program Files\Python37\Scripts\pyinstaller.exe" -y --clean --workpath .\build_temp --name test_runner --onedir --uac-uiaccess --hidden-import lackey --hidden-import pyautogui main.py
+python "F:\Program Files\Python37\Scripts\pyinstaller.exe"  -y --clean --workpath .\build_temp --name test_runner --onedir --uac-uiaccess --hidden-import pyautogui main.py
