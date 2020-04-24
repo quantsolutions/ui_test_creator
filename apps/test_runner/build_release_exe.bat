6c6b89981d48535b6a3c66f57984a69c7952575d
Margrit Drescher <margrit2103@gmail.com>
2020-04-24 17:33:20 +0200

    Test post commit hook

--- apps/test_runner/build_release_exe.bat
+++ apps/test_runner/build_release_exe.bat
@@ -1,5 +1,5 @@
 set mydate=%date:~6,4%-%date:~3,2%-%date:~0,2%
-set rar_name= ..\tingus_setup.exe
+set rar_name= ..\_tingus_setup.exe
 set cd_dir=dist
 set source_dir=test_runner
 echo %rar_name%
@@ -10,4 +10,11 @@ pause
 echo Version %mydate% > %source_dir%\readme.txt
 
 "c:\Program Files\WinRAR\WinRAR.exe" a -sfx %rar_name% %source_dir%
+
+
+REM.... - TEMP - ROLLED OUT IN goodxtests repository . Need to find a better solution
+md f:\github\goodxtests\desktop_app_tests\TingusInstall
+copy ..\_tingus_setup.exe f:\github\goodxtests\desktop_app_tests\TingusInstall
+
+
 pause
